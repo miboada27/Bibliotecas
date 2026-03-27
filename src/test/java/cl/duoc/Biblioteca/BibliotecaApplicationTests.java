90128f94c5d67d06de470b9b1424335cb6a23f0f
miboada27 <mi.boada@duocuc.cl>
2026-03-27 16:21:23 -0300

    avance

---
+++ src/test/java/cl/duoc/Biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.Biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
